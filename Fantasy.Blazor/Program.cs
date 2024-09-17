d2a5a2acafde207cc5a8fa48af8267e4af937bca
ShellShocker08 <mlunac08.30@hotmail.com>
2024-09-17 10:36:59 -0600

    06 - Creando el repositorio en el frontend

--- Fantasy.Blazor/Program.cs
+++ Fantasy.Blazor/Program.cs
@@ -1,4 +1,5 @@
 using Fantasy.Blazor;
+using Fantasy.Common.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//API
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7181") });
+
+//Helpers
+builder.Services.AddScoped<IApiService, ApiService>();
+
 
 await builder.Build().RunAsync();
---
+++ Fantasy.Common/Services/IApiService.cs
@@ -0,0 +1,12 @@
+﻿
+
+using Fantasy.Common.Models;
+
+namespace Fantasy.Common.Services;
+
+public interface IApiService
+{
+    Task<HttpResponseWrapper<T>> GetAsync<T>(string url);
+    Task<HttpResponseWrapper<object>> PostAsync<T>(string url, T model);
+    Task<HttpResponseWrapper<TActionResponse>> PostAsync<T, TActionResponse>(string url, T model);
+}
