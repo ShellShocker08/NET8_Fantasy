efd3ab2850a28f6959be7add82b48457f05aa82a
ShellShocker08 <mlunac08.30@hotmail.com>
2024-09-25 17:42:57 -0600

    08 - Creando Componente Generico de Listas

---
+++ Fantasy.Blazor/Pages/Shared/GenericList.razor.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components;
+
+namespace Fantasy.Blazor.Pages.Shared;
+
+public partial class GenericList<Titem>
+{
+    [Parameter] public RenderFragment? Loading { get; set; }
+    [Parameter] public RenderFragment? NoRecords { get; set; }
+    [EditorRequired, Parameter] public RenderFragment Body { get; set; } = null!;
+    [EditorRequired, Parameter] public List<Titem> MyList { get; set; } = null!;
+
+}
